f4c93288c86ec69fa36b6a406819a6f4a12db941
nina <ninagbs@abv.bg>
2023-06-15 20:20:14 +0300

    added Photo model and photoController

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const userController = require('./controllers/userController')
+const photoController = require('./controllers/photoController')
 //add controller routes
 // homeController is for main route "" and all static pages - home, about, 404
 router.use(homeController);
 router.use('/users', userController);
+router.use('/photos', photoController);
 
 
 router.use('*', (req, res)=>{
