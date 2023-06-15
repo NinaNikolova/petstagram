e59cccf8c40bc8629e8de762e006cd9a5b6d9d70
nina <ninagbs@abv.bg>
2023-06-15 21:08:37 +0300

    Add create page and create a pet

--- src/controllers/photoController.js
+++ src/controllers/photoController.js
@@ -1,7 +1,29 @@
 const router = require('express').Router();
+const photoManager = require('../managers/photoManager');
+const { getErrorMessage } = require('../utils/errorHelpers');
 
-router.get('/create', (req, res)=>{
+router.get('', async(req, res) => {
+    try {
+         const photos = await photoManager.getAll()
+    res.render('photos/catalog', photos)
+    } catch (err) {
+        res.render('photos/catalog', {error: getErrorMessage(err)})
+    }
+   
+})
+router.get('/create', (req, res) => {
     res.render('photos/create')
+})
+router.post('/create', async (req, res) => {
+    const photoData = req.body;
+    const owner = req.user._id;
+    try {
+        await photoManager.create({ ...photoData, owner });
+        res.redirect('/photos')
+    } catch (err) {
+res.render('photos/create', {error: getErrorMessage(err)})
+    }
+
 })
 
 module.exports = router;
